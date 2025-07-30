4f9a867cf18e5eeffa872464799c4608be90e74f
GitanjaliShelke03 <gitshelke@gmail.com>
2025-07-30 13:26:49 +0530

    Home page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
 
